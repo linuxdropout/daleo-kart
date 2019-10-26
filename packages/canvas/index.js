ccaf0050c8bfb17597d4b16e725a64f35fc71f17
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2019-10-26 17:07:43 +0100

    Camera tracking and world constrained player

--- packages/canvas/index.js
+++ packages/canvas/index.js
@@ -2,6 +2,13 @@ const GLOBALS = {
     scale: 0,
     msPerUpdate: 1000 / 60,
     GAME_OBJECTS: [],
+    player: null,
+    world: {
+        bottom: -1000,
+        top: 1000,
+        left: -1000,
+        right: 1000,
+    },
 }
 
 class GameObject {
@@ -12,6 +19,7 @@ class GameObject {
         this.y = y
         this.w = w
         this.h = h
+        this.coefResitution = 0.6
         this.color = 'white'
 
         this.dx = 0
@@ -20,6 +28,22 @@ class GameObject {
         this.ddy = 0
 
         GLOBALS.GAME_OBJECTS.push(this)
+    }
+
+    get top() {
+        return this.y + this.h
+    }
+
+    set top(value) {
+        this.y = value - this.h
+    }
+
+    get right() {
+        return this.x + this.w
+    }
+
+    set right(value) {
+        this.x = value - this.w
     }
 
     accelerate({ x = 0, y = 0 } = {}) {
@@ -34,6 +58,26 @@ class GameObject {
         this.dy += this.ddy
         this.y += this.dy
         this.ddy = 0
+
+        if (Math.abs(this.dy) < 1) this.dy = 0
+        if (Math.abs(this.dx) < 1) this.dx = 0
+
+        if (this.x < GLOBALS.world.left) {
+            this.x = GLOBALS.world.left
+            this.dx = this.dx * -this.coefResitution
+        }
+        if (this.right > GLOBALS.world.right) {
+            this.right = GLOBALS.world.right
+            this.dx = this.dx * -this.coefResitution
+        }
+        if (this.y < GLOBALS.world.bottom) {
+            this.y = GLOBALS.world.bottom
+            this.dy = this.dy * -this.coefResitution
+        }
+        if (this.top > GLOBALS.world.top) {
+            this.top = GLOBALS.world.top
+            this.dy = this.dy * -this.coefResitution
+        }
     }
 
     draw(ctx) {
@@ -59,9 +103,24 @@ function update() {
     }
 }
 
+function drawBackground(ctx) {
+    const {
+        top,
+        bottom,
+        left,
+        right,
+    } = GLOBALS.world
+    ctx.drawImage(GLOBALS.backgroundImage, left, bottom, right - left, top - bottom)
+}
+
 function draw(c, ctx) {
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, c.width, c.height)
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.clearRect(0, 0, c.width, c.height)
+    const camX = -GLOBALS.player.x + c.width / 2
+    const camY = -GLOBALS.player.y + c.height / 2
+    ctx.translate(camX, camY)
+
+    drawBackground(ctx)
     for (const object of GLOBALS.GAME_OBJECTS) {
         object.draw(ctx, c)
     }
@@ -94,21 +153,42 @@ function setupKeyBindings(player) {
     })
 }
 
-function main() {
+async function cacheImages(images) {
+    const imagePromises = []
+    for (const image of images) {
+        imagePromises.push(
+            new Promise(resolve => {
+                const img = new Image()
+                img.src = image
+                img.onload = () => resolve(img)
+            }),
+        )
+    }
+    return Promise.all(imagePromises)
+}
+
+async function main() {
+    const [
+        backgroundImage,
+    ] = await cacheImages([
+        'face.jpg',
+    ])
+    GLOBALS.backgroundImage = backgroundImage
+
     const c = document.getElementById('canvas')
     setScale(c)
     window.addEventListener('resize', () => setScale(c))
     document.addEventListener('resize', () => setScale(c))
     const ctx = c.getContext('2d')
 
-    const player = new GameObject({
+    GLOBALS.player = new GameObject({
         x: 50,
         y: 50,
         w: 50,
         h: 50,
     })
 
-    setupKeyBindings(player)
+    setupKeyBindings(GLOBALS.player)
 
     loop(c, ctx, GLOBALS.msPerUpdate)
 }
