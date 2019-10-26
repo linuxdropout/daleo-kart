703a11def2371bb191dda217ce78cde1e703dc06
Wedge <matthew.wedgeroberts@netacea.com>
2019-10-26 15:44:10 +0100

    sockets for multiplayer and scores

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,29 +1,38 @@
-const app = require('express')()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-const allScores = []
+const allPlayers = []
+
+app.use(bodyParser())
 
 app.get('/', (req, res) => {
+    console.log(dirname + '/index.html')
     res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/register-player', (req, res, next) => {
-    const playerName = req.query.name
-    allScores.push({
+    const playerName = req.body.name
+    const playerData = {
         name: playerName,
         score: 0
-    })
+    }
+    allPlayers.push(playerData)
+    io.sockets.emit('newPlayer', playerData)
     res.json({
-        success: true
+        success: true,
+        allPlayers
     })
 })
 
-io.on('connection', socket => {
-    console.log('CONNECTION')
-})
+console.log('STARTED')
 
-io.on('')
+// io.on('connection', socket => {
+//     console.log('CONNECTION')
+//     io.sockets.emit('newConnection', allPlayers)
+// })
 
 http.listen(3000, function(){
     console.log('listening on *:3000')
